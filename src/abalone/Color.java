f91d60ecf53b840232613cad46cec94918122763
Daan Pluister <d.pluister@student.utwente.nl>
2020-01-16 16:11:23 +0100

    aslo the color from previous commit

--- src/abalone/Color.java
+++ src/abalone/Color.java
@@ -11,88 +11,85 @@ public enum Color {
 	private Boolean clockwise;
 
 	public Color next(int numOfPlayers) {
-    	switch (numOfPlayers) {
-	    	case 2:
-	    		switch (this) {
-		    		case WHITE:
-		    			return BLACK;
-	    			default:
-	    				return WHITE;
-	    		}
-	    	default:
-	    		if (clockwise == null) {
-	            	clockwise = (Math.random() < 0.5);
-	        	}
-	    		if (clockwise) {
-	    			switch (this) {
-		    		case BLUE:
-		    			return WHITE;
-		    		case BLACK:
-		    			return BLUE;
-	    			case RED:
-	    				return BLACK;
-	    			default: //WHITE
-	    				if (numOfPlayers == 4) {
-		    				return RED;
-		    			}
-		    			return BLACK;
-	    			}	
-	    		} else {
-	    			switch (this) {
-		    		case BLUE:
-		    			return BLACK;
-		    		case WHITE:
-		    			return BLUE;
-	    			case RED:
-	    				return WHITE;
-	    			default: //BLACK
-	    				if (numOfPlayers == 4) {
-		    				return RED;
-		    			}
-		    			return WHITE;
-	    			}	
-    			}
-    		}
-	
-    	/**
-    	 * 
-    	 * @return (WHITE,BLACK,BLUE,RED) => (1,2,3,4)
-    	 */
-    	public int getInt() {
-    		switch (this) {
-    		case WHITE:
-    			return 1;
-    		case BLACK:
-    			return 2;
-    		case BLUE:
-    			return 3;
-    		case RED:
-    			return 4;
-    		default:
-    			return 0;
-    		}
-    	}
-    	
-
-    	/**
-    	 * toString method of enum Marble.
-    	 */
-    	public String toString() {
-    		switch (this) {
-    		case WHITE:
-    			return "W";
-    		case BLACK:
-    			return "B";
-    		case BLUE:
-    			return "P";
-    		case RED:
-    			return "R";
-    		default:
-    			return " ";
-    		}
-    	}
-
+		switch (numOfPlayers) {
+		case 2:
+			switch (this) {
+			case WHITE:
+				return BLACK;
+			default:
+				return WHITE;
+			}
+		default:
+			if (clockwise == null) {
+				clockwise = (Math.random() < 0.5);
+			}
+			if (clockwise) {
+				switch (this) {
+				case BLUE:
+					return WHITE;
+				case BLACK:
+					return BLUE;
+				case RED:
+					return BLACK;
+				default: // WHITE
+					if (numOfPlayers == 4) {
+						return RED;
+					}
+					return BLACK;
+				}
+			} else {
+				switch (this) {
+				case BLUE:
+					return BLACK;
+				case WHITE:
+					return BLUE;
+				case RED:
+					return WHITE;
+				default: // BLACK
+					if (numOfPlayers == 4) {
+						return RED;
+					}
+					return WHITE;
+				}
+			}
+		}
 	}
 
-	
-	
+	/**
+	 * 
+	 * @return (WHITE,BLACK,BLUE,RED) => (1,2,3,4)
+	 */
+	public int getInt() {
+		switch (this) {
+		case WHITE:
+			return 1;
+		case BLACK:
+			return 2;
+		case BLUE:
+			return 3;
+		case RED:
+			return 4;
+		default:
+			return 0;
+		}
+	}
+
+	/**
+	 * toString method of enum Marble.
+	 */
+	public String toString() {
+		switch (this) {
+		case WHITE:
+			return "W";
+		case BLACK:
+			return "B";
+		case BLUE:
+			return "P";
+		case RED:
+			return "R";
+		default:
+			return " ";
+		}
+	}
+
+}
