4df45b0f7ab39e60a6617768e2daf50f7417680c
s2020343 <b.p.petrov@student.utwente.nl>
2020-01-21 16:09:26 +0100

    actually added random strategy

--- src/abalone/AI/RandomStrategy.java
+++ src/abalone/AI/RandomStrategy.java
@@ -1,19 +1,24 @@
 package abalone.AI;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 import abalone.Board;
 import abalone.Color;
 import abalone.Move;
 
 public class RandomStrategy  implements Strategy {
-	
+
 	public String getName() {
 		return "Random";
 	}
 
 	@Override
-	public Move determineMove(Board board, Color mark) {
-		// TODO Auto-generated method stub
-		return null;
+	public Move determineMove(Board board, Color color) {
+		ArrayList<Move> moveList = makeMovesList(board, color);
+		int random = new Random().nextInt(moveList.size());
+		return moveList.get(random);
 	}
 	
 }
